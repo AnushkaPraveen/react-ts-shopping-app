20894311c6df12ee8e1dbcbb318dc68bb801ec32
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-09-15 14:21:14 +0530

    signIn component add

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './components/Home'
+import SignIn from './components/SignIn'
 
 function App() {
 
   return (
     <>
      <Header/>
-     <Home/>
+     {/* <Home/> */}
+     <SignIn/>
      <Footer/>
     </>
   )
