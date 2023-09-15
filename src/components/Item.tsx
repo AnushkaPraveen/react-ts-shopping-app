cabeb6f2db7808dee27612677b6f6d393f7424ca
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-09-15 09:55:32 +0530

    product item UI design

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,7 +2,7 @@ import classes from './Item.module.css';
 const Item = () => {
     return (
         <>
-            <div className='mx-2'>
+            <div className='mx-2 my-5'>
                 <span className="badge bg-secondary position-absolute mt-3 ms-3">New</span>
                 <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" className={classes.productItemImage} />
                 <div className='d-flex justify-content-between align-items-center mt-2'>
