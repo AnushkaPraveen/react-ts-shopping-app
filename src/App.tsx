55cd7f2fa6aafb294534a2916665225631d81590
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-09-22 00:08:13 +0530

    component change

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 /* import CartItem from './components/modules/CartItem'; */
 import Footer from './components/Footer';
 import Header from './components/Header';
-/* import Home from './components/Home';
-import ProfileView from './pages/ProfileView';
+import Home from './pages/Home';
+/* import ProfileView from './pages/ProfileView';
 import SignIn from './components/SignIn';
 import Cart from './pages/Cart'; */
 import ProductDetailsView from './pages/ProductDetailsView';
@@ -13,11 +13,11 @@ function App() {
   return (
     <>
      <Header/>
-     {/* <Home/> */}
+     <Home/> 
     {/*  <SignIn/> */}
     {/* <ProfileView/> */}
    {/*  <Cart/> */}
-   <ProductDetailsView/>
+   {/* <ProductDetailsView/> */}
      <Footer/>
     </>
   )
