90ca9f6c05e7ad884f1f3b559f1260e9921eb860
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-09-22 12:28:47 +0530

    api type change response type change seprate response type file

--- src/api/productApi.tsx
+++ src/api/productApi.tsx
@@ -1,29 +1,7 @@
 
 import axios, { AxiosResponse } from 'axios';
-
-type ProductType = {
-  id: number;
-  title: string;
-  description: string;
-  price: number;
-  discountPercentage: number;
-  rating: number;
-  stock: number;
-  brand: string;
-  category: string;
-  thumbnail: string;
-  images: string[];
-};
-
-type ApiResponseType = {
-  products: ProductType[];
-  total: number;
-  skip: number;
-  limit: number;
-};
-
-type ProductCategoryType=string[]
-
+import { ProductType } from '../components/modules/product/productTypes';
+import { ProductApiResponse,ProductCategoryType } from './responseTypes';
 
 
 
@@ -42,10 +20,10 @@ const requests = {
 };
 
 export const Product = {
-  getProducts: (): Promise<ApiResponseType[]> => requests.get('?limit=12&skip=0'),
+  getProducts: (): Promise<ProductApiResponse> => requests.get('?limit=12&skip=0'),
   getCategory: (): Promise<ProductCategoryType> => requests.get('categories'),
-  getProductListByCategory: (): Promise<ApiResponseType> => requests.get('category/smartphones?limit=10&skip=0'),
-  getSearchProduct: (): Promise<ApiResponseType> => requests.get('https://dummyjson.com/products/search?q=phone'),
+  getProductListByCategory: (): Promise<ProductType> => requests.get('category/smartphones?limit=10&skip=0'),
+  getSearchProduct: (): Promise<ProductType> => requests.get('https://dummyjson.com/products/search?q=phone'),
   createPost: (post: ProductType): Promise<ProductType> =>
     requests.post('posts', post),
   updatePost: (post: ProductType, id: number): Promise<ProductType> =>
