9747851073fd501154ec93861f4428ee40e1b0fd
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-09-25 09:48:52 +0530

    routes add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 //import './App.css'
-/* import CartItem from './components/modules/CartItem'; */
+import { Routes, Route } from "react-router-dom"
+//import CartItem from './components/modules/CartItem'; */
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
-/* import ProfileView from './pages/ProfileView';
-import SignIn from './components/SignIn';
-import Cart from './pages/Cart'; */
+import ProfileView from './pages/ProfileView';
+import SignIn from './components/modules/user/SignIn';
+import Cart from './pages/Cart';
 import ProductDetailsView from './pages/ProductDetailsView';
 
 function App() {
@@ -13,7 +14,15 @@ function App() {
   return (
     <>
      <Header/>
-     <Home/> 
+     <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="services/:productId" element={ <ProductDetailsView/> } />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/profile" element={<ProfileView/>} />
+        <Route path="/login" element={<SignIn/>} />
+      </Routes>
+  
     {/*  <SignIn/> */}
     {/* <ProfileView/> */}
    {/*  <Cart/> */}
