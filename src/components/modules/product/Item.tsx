e58286fbe3b6ef4b840b64c444c9c0a174a53516
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-09-27 01:27:29 +0530

    cart add function add

--- src/components/modules/product/Item.tsx
+++ src/components/modules/product/Item.tsx
@@ -1,28 +1,38 @@
 import { Link } from "react-router-dom";
+import { useAppSelector, useAppDispatch } from '../../../store/hooks';
 import classes from './Item.module.css';
 import { ProductType } from "./productTypes";
+import { cartActions } from '../../../store/slices/cartSlice';
 
 type ComponentProps = {
     product: ProductType;
 }
 
 const Item: React.FC<ComponentProps> = ({ product }) => {
+    const dispatch = useAppDispatch();
+    const products = useAppSelector((state) => state.cart.productsCart);
+    console.log(products);
+    
+
+    const handleCart = () => {
+        dispatch(cartActions.addItemToCart(product));
+    }
 
     return (
         <>
             <div className='mx-2 my-5'>
                 <Link to={`/services/${product.id}`}>
-                <span className="badge bg-secondary position-absolute mt-3 ms-3">New</span>
-                <img src={product.thumbnail} className={classes.productItemImage}/>
+                    <span className="badge bg-secondary position-absolute mt-3 ms-3">New</span>
+                    <img src={product.thumbnail} className={classes.productItemImage} />
                 </Link>
-                
+
                 <div className='d-flex justify-content-between align-items-center mt-2'>
                     <div >
                         <span className={classes.productName}>{product.title}</span><br />
                         <span className={classes.productPrice}>${product.price}</span>
                         <span className={classes.productSalePrice}>$20</span>
                     </div>
-                    <button className={`${classes.productCartButton} btn`}><i className="bi bi-cart"></i></button>
+                    <button className={`${classes.productCartButton} btn`} onClick={handleCart}><i className="bi bi-cart"></i></button>
                 </div>
             </div>
         </>
