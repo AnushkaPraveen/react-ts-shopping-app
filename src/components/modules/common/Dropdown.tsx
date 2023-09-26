6adaa52a7f4ec346483ce5ee534716b01fc5ccee
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-09-26 23:58:36 +0530

    category product render function add

--- src/components/modules/common/Dropdown.tsx
+++ src/components/modules/common/Dropdown.tsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../../../store/hooks';
+import { fetchProductsByCategory } from '../../../store/actions/productAction';
+import { commonActions } from '../../../store/slices/commonSlice';
+
 const Dropdown = () => {
-    const categories= useAppSelector((state) => state.product.categories);
+    const dispatch = useAppDispatch();
+    const [selectedcategory, setSelectedCategory] = useState<string>("All categories")
+    const categories = useAppSelector((state) => state.product.categories);
+
+    const handleCategory = async (categoryName: string) => {
+        dispatch(commonActions.setLoader(true));
+        setSelectedCategory(categoryName)
+        await dispatch(fetchProductsByCategory(categoryName));
+        dispatch(commonActions.setLoader(false));
+
+    }
+
     return (
         <>
             <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                All Categories
+                {selectedcategory}
             </button>
             <ul className="dropdown-menu">
-                {categories.map((item,index)=>(
-                    <li key={index}><a className="dropdown-item" href="#">{item}</a></li>
+                {categories.map((item, index) => (
+                    <li key={index}><a className="dropdown-item" href="#" onClick={() => handleCategory(item)}>{item}</a></li>
                 ))
                 }
             </ul>
