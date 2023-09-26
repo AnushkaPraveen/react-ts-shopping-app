24345bfd3ab7ac7151898afd66c7c9835f67c3dc
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-09-26 23:58:10 +0530

    search function add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
+import { useState, ChangeEvent } from 'react';
+import { useAppSelector, useAppDispatch } from '../store/hooks';
+import { Link } from 'react-router-dom';
 import classes from './Header.module.css';
 import Dropdown from './modules/common/Dropdown';
+import { fetchSearchProduct } from '../store/actions/productAction';
 
 const Header: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const [searchText, setSearchText] = useState<string>('');
+
+    const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+        const inputValue: string = event.target.value;
+        setSearchText(inputValue);
+    }
+
+    const handleSearchProduct = async () => {
+        await dispatch(fetchSearchProduct(searchText));
+    }
 
     return (
         <>
@@ -11,27 +26,35 @@ const Header: React.FC = () => {
                 </div>
             </div>
             <nav className={`${classes.navBar} d-flex justify-content-evenly align-items-center navbar navbar-expand-lg`}>
-                <p className={classes.navMainTitle}><i className="bi bi-shop text-info me-2"></i>Comforty</p>
+                <Link to="/" className={classes.linkTextDecoration}>
+                    <p className={classes.navMainTitle}><i className="bi bi-shop text-info me-2"></i>Comforty</p>
+                </Link>
+
                 <div className='d-flex'>
                     <div className="dropdown me-2">
-                        <Dropdown/>
+                        <Dropdown />
                     </div>
                     <div className="input-group me-2">
-                        <input className="form-control  border-right-0 border" type="search" placeholder="Search" id="example-search-input" />
+                        <input className="form-control  border-right-0 border" type="search" placeholder="Search" id="example-search-input" onChange={handleInput} />
                         <span className="input-group-append">
                             <div className="input-group-text bg-white"><i className="bi-search"></i></div>
                         </span>
                     </div>
-                    <button className={`${classes.buttonColor} btn text-white`} type="submit">Search</button>
+                    <button className={`${classes.buttonColor} btn text-white`} type="submit" onClick={handleSearchProduct}>Search</button>
                 </div>
                 <div className='d-flex mt-sm-2 mt-lg-0 mt-md-0'>
-                    <button type="button" className="btn btn-light me-4">
-                        <i className="bi bi-cart me-2 fs-5"></i>
-                        Cart <span className={`${classes.buttonColor} badge ms-2`}>4</span>
-                    </button>
-                    <button type="button" className="btn btn-light">
-                        <i className="bi bi-person fs-5"></i>
-                    </button>
+                    <Link to="/cart">
+                        <button type="button" className="btn btn-light me-4">
+                            <i className="bi bi-cart me-2 fs-5"></i>
+                            Cart <span className={`${classes.buttonColor} badge ms-2`}>4</span>
+                        </button>
+                    </Link>
+                    <Link to="/login">
+                        <button type="button" className="btn btn-light">
+                            <i className="bi bi-person fs-5"></i>
+                        </button>
+                    </Link>
+
                 </div>
             </nav>
         </>
