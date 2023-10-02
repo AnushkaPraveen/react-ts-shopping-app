18a3c42c0620c8af2cc4a8600e9bd5a8111a517c
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-02 16:14:58 +0530

    total amount total tax clear cart checkout cart functions add

--- src/store/slices/cartSlice.tsx
+++ src/store/slices/cartSlice.tsx
@@ -2,12 +2,16 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ProductType } from '../../components/modules/product/productTypes';
 
 type CartState = {
-    categories: string[];
-    productsCart: ProductType[]
+    productsCart: ProductType[],
+    cartTotalAmount: number,
+    totalTaxAmount: number,
+    checkOut: boolean
 }
 const initialState: CartState = {
-    categories: [],
-    productsCart: []
+    productsCart: [],
+    cartTotalAmount: 0,
+    totalTaxAmount: 0,
+    checkOut: false
 };
 
 const cartSlice = createSlice({
@@ -15,7 +19,14 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addItemToCart: (state, action: PayloadAction<ProductType>) => {
-            state.productsCart.push(action.payload);
+            const itemIndex = state.productsCart.findIndex(item => item.id === action.payload.id);
+            if (itemIndex >= 0) {
+                state.productsCart[itemIndex].cartQuantity += 1;
+            } else {
+                const tempProduct = { ...action.payload, cartQuantity: 1 };
+                state.productsCart.push(tempProduct);
+            }
+
         },
         removeItemFromCart: (state, action: PayloadAction<number>) => {
             const itemIndex = state.productsCart.findIndex(item => item.id === action.payload);
@@ -23,6 +34,38 @@ const cartSlice = createSlice({
                 state.productsCart.splice(itemIndex, 1);
             }
         },
+        getToatlAmount: (state) => {
+            let { total, quantity } = state.productsCart.reduce(
+                (cartTotal, cartItem) => {
+                    const { price, cartQuantity } = cartItem;
+                    const itemTotal = price * cartQuantity;
+
+                    cartTotal.total += itemTotal;
+                    cartTotal.quantity += cartQuantity;
+
+                    return cartTotal;
+                },
+                {
+                    total: 0,
+                    quantity: 0,
+                }
+            );
+            total = parseFloat(total.toFixed(2));
+            //state.cartTotalQuantity = quantity;
+            state.cartTotalAmount = total;
+        },
+        getTotalTax: (state) => {
+            const tax = (state.cartTotalAmount * 6.5) / 100;
+            state.totalTaxAmount = parseFloat(tax.toFixed(2));
+        },
+        checkoutCart: (state, action) => {
+            state.checkOut = action.payload;
+        },
+        clearCart: (state, action) => {
+            state.productsCart = [];
+            state.cartTotalAmount = 0;
+            state.totalTaxAmount = 0;
+        }
     }
 
 });
