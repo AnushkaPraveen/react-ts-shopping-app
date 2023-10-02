1d8b19f8e1bab3f342828267071921fca89cc6c8
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-02 16:16:45 +0530

    display items checkout button handle

--- src/components/modules/cart/TotalAmount.tsx
+++ src/components/modules/cart/TotalAmount.tsx
@@ -1,26 +1,33 @@
 import classes from './TotalAmount.module.css';
 import CommonClasses from '../../../util/common.module.css'
-const TotalAmount = () => {
+
+type ComponentProps = {
+    totalAmount: number;
+    totalTaxAmount:number;
+    handleCheckout:()=>void;
+}
+
+const TotalAmount: React.FC<ComponentProps>  = ({totalAmount,totalTaxAmount,handleCheckout}) => {
     return (
         <div className={classes.amountContainer}>
             <div className="row">
                 <div className="d-flex justify-content-between">
-                    <p>subtotal</p>
-                    <p>$129.00</p>
+                    <p>subtotal </p>
+                    <p>${totalAmount.toFixed(2)}</p>
                 </div>
 
                 <hr />
                 <div className="d-flex justify-content-between">
                     <p>Sales tax(6.5%)</p>
-                    <p>$4.00</p>
+                    <p>${totalTaxAmount}</p>
                 </div>
 
                 <hr />
                 <div className="d-flex justify-content-between">
                     <p>Total due</p>
-                    <p>$129.00</p>
+                    <p>${totalAmount-totalTaxAmount}</p>
                 </div>
-                <button className={`${CommonClasses.buttonColor} btn`}>Check Out</button>
+                <button className={`${CommonClasses.buttonColor} btn`} onClick={handleCheckout}>Check Out</button>
             </div>
         </div>
 
