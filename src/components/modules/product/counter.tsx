ea448fb42637513701d4f6ed96f437b583ce5e1c
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-03 15:32:14 +0530

    counter component add

--- src/components/modules/product/counter.tsx
+++ src/components/modules/product/counter.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import Classes from './counter.module.css';
-const Counter = () => {
+
+type ComponentProps = {
+    count: number ;
+    increaseQuantity:(newCount: number) =>void;
+    decreaseQuantity:(newCount: number) =>void;
+}
+
+
+const Counter: React.FC<ComponentProps> = ({count,increaseQuantity,decreaseQuantity}) => {
+    //const [count, setCount] = useState<number>(1);
+
+
+    const increaseCount = () => {
+        const newCount: number  = count + 1;
+        increaseQuantity(newCount); 
+    };
+
+
+    const decreaseCount = () => {
+        if (count > 1) {
+            const newCount: number  = count - 1;
+            decreaseQuantity(newCount); // Call the callback to update the count in the parent
+        }
+    };
     return (
         <>
-            <button className={`${Classes.updownbtn}`}>-</button>
-            <span className={Classes.displayCount}>1</span>
-            <button className={`${Classes.updownbtn} me-5`}>+</button>
+            <button className={`${Classes.updownbtn}`} onClick={decreaseCount}>-</button>
+            <span className={Classes.displayCount}>{count}</span>
+            <button className={`${Classes.updownbtn} me-5`} onClick={increaseCount}>+</button>
         </>
     )
 }
