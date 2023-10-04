3ab498a103e493b1ba27ce8ac3340e17d7bddf23
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-05 01:10:50 +0530

    seprate types

--- src/store/actions/cartAction.tsx
+++ src/store/actions/cartAction.tsx
@@ -1,28 +1,7 @@
-import { productActions } from "../slices/productSlice";
 import { Cart } from "../../api/cartApi";
-import { useAppSelector, useAppDispatch } from '../hooks';
-import { useSelector } from 'react-redux';
-import { ProductType } from "../../components/modules/product/productTypes";
-//import { ProductApiResponse } from "../../api/responseTypes";
-//import { Dispatch } from 'redux';
-//const cartProducts = useSelector((state) => state.cart.productsCart);
-//console.log("cart action",cartProducts);
-type CartItem = {
-    id: number | string;
-    quantity: number | string;
-}
-type CartData = {
-    userId: number;
-    products: CartItem[];
-}
+import { CartData } from "../../api/responseTypes";
 
 export const createProductCart = async (cartProducts: CartData) => {
-
- 
-
-  
-
-
     try {
         const productCategoryData = await Cart.createCart(cartProducts);
         console.log(productCategoryData);
@@ -30,5 +9,4 @@ export const createProductCart = async (cartProducts: CartData) => {
     } catch (error) {
         console.log(error);
     }
-
 }
