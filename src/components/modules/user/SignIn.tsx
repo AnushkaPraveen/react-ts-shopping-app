60ff2237e2f45d0d3ba0a5c17a30848920b41eb8
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-05 01:10:37 +0530

    handlelogin function add

--- src/components/modules/user/SignIn.tsx
+++ src/components/modules/user/SignIn.tsx
@@ -1,6 +1,20 @@
 import classes from '../../../util/common.module.css';
+import { loginUser } from '../../../store/actions/userAction';
+import { useAppSelector, useAppDispatch } from '../../../store/hooks';
 
 const SignIn = () => {
+    const dispatch = useAppDispatch();
+     type UserDetails = {
+        username: string;
+        password: string;
+    }
+    const handleLogin = async () => {
+        const userDetails:UserDetails = {
+            username: "kminchelle",
+            password: "0lelplR"
+        }
+        //dispatch(loginUser(userDetails));
+    }
     return (
         <>
             <div className="d-flex justifiy-content-center">
@@ -9,12 +23,12 @@ const SignIn = () => {
                     <input type="email" className="form-control mb-3" id="exampleFormControlInput1" placeholder="Email" />
                     <input type="password" id="inputPassword5" className="form-control mb-3" aria-describedby="passwordHelpBlock" placeholder="Password" />
                     <div className="form-check mb-3">
-                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label className="form-check-label">
-                                Remember Me
-                            </label>
+                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                        <label className="form-check-label">
+                            Remember Me
+                        </label>
                     </div>
-                    <button className={`${classes.buttonColor} btn`}>Sign In</button>
+                    <button className={`${classes.buttonColor} btn`} onClick={handleLogin}>Sign In</button>
                 </div>
             </div>
         </>
