d798d378a9122d977d790c8a16a38268e3f3a081
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-05 23:31:40 +0530

    login action user profile action add

--- src/store/actions/userAction.tsx
+++ src/store/actions/userAction.tsx
@@ -1,20 +1,35 @@
 import { User } from "../../api/userApi";
-import { UserDetails } from "../../api/responseTypes";
+import { UserDetails, } from "../../api/responseTypes";
 import { userActions } from "../slices/userSlice";
-import { Dispatch } from 'redux';
-import { AppDispatch } from "../store";
 
-export const loginUser =  async(userDetails: UserDetails) => {
-    console.log(userDetails);
-    
-    return async (dispatch: Dispatch) => {
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+ export const loginUser = createAsyncThunk(
+    'loginUser',
+    async (userDetails:UserDetails,thunkAPI) => {
         try {
             console.log("inside");
             const userData = await User.loginUser(userDetails);
             console.log(userData);
-            dispatch(userActions.setUser(userData));
+            thunkAPI.dispatch(userActions.setUser(userData));
+            localStorage.setItem("user", JSON.stringify(userData));
         } catch (error) {
             console.log(error);
         }
     }
-}
+  ); 
+
+  export const getUserProfile = createAsyncThunk(
+    'getuser',
+    async (userId:number,thunkAPI) => {
+        try {
+            console.log("inside");
+            const userData = await User.getUserDetails(userId);
+            console.log(userData);
+            thunkAPI.dispatch(userActions.setUserProfile(userData));
+            //localStorage.setItem("user", JSON.stringify(userData));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+  ); 
