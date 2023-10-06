1c97ec442f9fd3a765eeb70341a575625606efa4
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-06 16:17:28 +0530

    toast message add

--- src/store/actions/cartAction.tsx
+++ src/store/actions/cartAction.tsx
@@ -1,12 +1,15 @@
 import { Cart } from "../../api/cartApi";
 import { CartData } from "../../api/responseTypes";
+import { toast } from "react-toastify";
 
 export const createProductCart = async (cartProducts: CartData) => {
     try {
         const productCategoryData = await Cart.createCart(cartProducts);
         console.log(productCategoryData);
+        toast.success("Your puchase has been confirmed",{position:'top-center',theme: "colored",autoClose:3000});
         //dispatch(productActions.setCategories(productCategoryData));
     } catch (error) {
+        toast.error("Your puchase has been failed",{position:'top-center',theme: "colored",autoClose:3000});
         console.log(error);
     }
 }
