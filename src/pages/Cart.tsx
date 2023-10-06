794ec1f3e2ddf1cbd8da0f63aab6c1185180fdfb
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-06 16:17:19 +0530

    checkout function add

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom';
 import CartItem from "../components/modules/cart/CartItem";
 import TotalAmount from "../components/modules/cart/TotalAmount";
 import { useEffect } from "react";
+import { toast } from "react-toastify";
 import Payment from "../components/modules/cart/Payement";
 import Classes from './Cart.module.css';
 import { useAppSelector, useAppDispatch } from '../store/hooks';
@@ -8,10 +10,12 @@ import { cartActions } from "../store/slices/cartSlice";
 
 const Cart = () => {
     const dispatch = useAppDispatch();
+    const navigate = useNavigate();
     const cartProducts = useAppSelector((state) => state.cart.productsCart);
     const cartTotalAmount = useAppSelector((state) => state.cart.cartTotalAmount);
     const cartTotalTaxAmount = useAppSelector((state) => state.cart.totalTaxAmount);
     const checkout = useAppSelector((state) => state.cart.checkOut);
+    const user = JSON.parse(localStorage.getItem('user') as string);
     //console.log("total", cartTotalAmount, "tax", cartTotalTaxAmount);
 
 
@@ -20,15 +24,33 @@ const Cart = () => {
         dispatch(cartActions.getTotalTax());
     }, [cartProducts]);
 
+   /*  useEffect(() => {
+        if (user) {
+            dispatch(cartActions.checkoutCart(false));
+        }
+    }, [user]); */
+
     const handleCheckout = () => {
-        dispatch(cartActions.checkoutCart(true));
+        if (user) {
+            dispatch(cartActions.checkoutCart(true));
+            console.log("with user");
+
+
+        } else {
+            dispatch(cartActions.checkoutCart(false));
+            navigate("/login");
+            console.log("without user");
+
+        }
+
     }
 
     const removeItem = (itemId: number) => {
         dispatch(cartActions.removeItemFromCart(itemId))
+        toast.success("Product removed from cart", { position: 'top-center', theme: "colored", });
     }
     //console.log(cartProducts);
-    
+
     return (
         <>
             <div className="container mt-5 mb-5">
@@ -38,7 +60,7 @@ const Cart = () => {
                             <div className="col justify-content-center" key={index}>
                                 <CartItem cartItem={item} removeItemFromCart={removeItem} />
                             </div>
-                        )) : <div className="col justify-content-center"><h3 className="text-center">No Items yet</h3></div>}
+                        )) : <div className="col text-center"><h3 className="text-center">No Items yet</h3></div>}
                         <div className="container">
                             {cartProducts.length > 0 && (<div className={`${Classes.amountContainer} d-flex justify-content-end`}>
                                 <TotalAmount totalAmount={cartTotalAmount} totalTaxAmount={cartTotalTaxAmount} handleCheckout={handleCheckout} />
