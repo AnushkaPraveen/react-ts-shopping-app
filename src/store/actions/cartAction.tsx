6cf9cc31e6a7c2f70ba7feed615190fe687001a3
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-09 10:21:03 +0530

    add to cleanCart reducer

--- src/store/actions/cartAction.tsx
+++ src/store/actions/cartAction.tsx
@@ -1,15 +1,20 @@
 import { Cart } from "../../api/cartApi";
 import { CartData } from "../../api/responseTypes";
+import { cartActions } from "../slices/cartSlice";
 import { toast } from "react-toastify";
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const createProductCart = async (cartProducts: CartData) => {
-    try {
-        const productCategoryData = await Cart.createCart(cartProducts);
-        console.log(productCategoryData);
-        toast.success("Your puchase has been confirmed",{position:'top-center',theme: "colored",autoClose:3000});
-        //dispatch(productActions.setCategories(productCategoryData));
-    } catch (error) {
-        toast.error("Your puchase has been failed",{position:'top-center',theme: "colored",autoClose:3000});
-        console.log(error);
+export const createProductCart = createAsyncThunk(
+    'productCart',
+    async (cartProducts: CartData, thunkAPI) => {
+        try {
+            const productCategoryData = await Cart.createCart(cartProducts);
+            console.log(productCategoryData);
+            toast.success("Your puchase has been confirmed", { position: 'top-center', theme: "colored", autoClose: 3000 });
+            thunkAPI.dispatch(cartActions.clearCart());
+        } catch (error) {
+            toast.error("Your puchase has been failed", { position: 'top-center', theme: "colored", autoClose: 3000 });
+            console.log(error);
+        }
     }
-}
+)
