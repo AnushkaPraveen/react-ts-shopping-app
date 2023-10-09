31064c8d48a37152e08edccd3897c99444ce14d3
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-10-10 00:45:16 +0530

    toast message add

--- src/store/actions/userAction.tsx
+++ src/store/actions/userAction.tsx
@@ -3,6 +3,7 @@ import { UserDetails, } from "../../api/responseTypes";
 import { userActions } from "../slices/userSlice";
 import { commonActions } from "../slices/commonSlice";
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from "react-toastify";
 
 export const loginUser = createAsyncThunk(
     'loginUser',
@@ -14,8 +15,10 @@ export const loginUser = createAsyncThunk(
             thunkAPI.dispatch(userActions.setUser(userData));
             thunkAPI.dispatch(commonActions.setLogingUser(true));
             localStorage.setItem("user", JSON.stringify(userData));
-        } catch (error) {
-            console.log(error);
+            toast.success("Successfuly Logged", { position: 'top-center', theme: "colored", autoClose: 3000 });
+        } catch (error:any) {
+            console.log(error.response.data.message);
+            toast.error(error.response.data.message, { position: 'top-center', theme: "colored", autoClose: 1000 });
         }
     }
 );
@@ -31,6 +34,7 @@ export const getUserProfile = createAsyncThunk(
             //localStorage.setItem("user", JSON.stringify(userData));
         } catch (error) {
             console.log(error);
+            toast.error("Something went wrong", { position: 'top-center', theme: "colored", autoClose: 1000 });
         }
     }
 ); 
